5b5007e6ae5c2614f94b28d27da0249f70ad7625
Frederic Auberson <frederic@auberson.net>
2019-09-18 19:52:22 +0200

    Basic project skeleton

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -1,8 +1,25 @@
-// give the user a nice default project!
-ThisBuild / organization := "com.example"
-ThisBuild / scalaVersion := "2.12.8"
+name := "riot-streaming-demo"
+scalaVersion := "2.12.8"
 
-lazy val root = (project in file(".")).
-  settings(
-    name := "streams.g8"
+libraryDependencies ++= Seq(
+  "org.riot-framework" % "riot-core" % "0.2",
+  "ch.qos.logback" % "logback-classic" % "1.2.3"
+)
+
+lazy val root = (project in file("."))
+  .enablePlugins(JavaServerAppPackaging)
+  .settings(
+  
+    // Deployment Targets (hostname, username, password):
+    riotTargets := Seq(
+      riotTarget("raspberrypi", "pi", "raspberry")
+      ),
+      
+    // Port to use for remote debugging:
+    riotDbgPort := 8000,
+      
+    // Packages and features needed by your code:
+    riotPrereqs := "oracle-java8-jdk wiringpi",
+    riotRequiresI2C := false,
+    riotRequiresSPI := false
   )
---
+++ src/main/g8/src/main/java/Application.java
@@ -0,0 +1,35 @@
+import java.time.Duration;
+
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+
+/**
+ * A skeleton for a RIoT application: Create a timer that sends a "Tick" message
+ * each second, then print it to the console.
+ */
+public class Application {
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, InterruptedException {
+		ActorSystem system = ActorSystem.create("riot-streaming-demo");
+		Materializer mat = ActorMaterializer.create(system);
+
+		// Example: This timer source sends a String each second:
+		Source<String, ?> tickSource = Source.tick(Duration.ZERO, Duration.ofSeconds(1), "Tick!");
+
+		// This sink will output the values returned straight to the console:
+		Sink<String, ?> stdOutSink = Sink.foreach(o -> System.out.println(o));
+
+		// Streams are built by a Materializer (mat), and go from a source (via a number
+		// of flow steps) to a sink:
+		tickSource.to(stdOutSink).run(mat);
+
+		// Use RIoT's Builders (e.g. riot.GPIO, riot.I2C) or Akka Streams components
+		// from other projects (e.g. Alpakka) to create additional steps, and use them
+		// in your streams.
+
+	}
+
+}
