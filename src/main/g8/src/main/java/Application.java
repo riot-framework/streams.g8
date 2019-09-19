2f468eec1c02fd62289911cb10617e6da40f4a2a
Frederic Auberson <frederic@auberson.net>
2019-09-19 17:42:27 +0200

    Fixed filter switching spaces to tabs :/

--- src/main/g8/src/main/java/Application.java
+++ src/main/g8/src/main/java/Application.java
@@ -12,23 +12,23 @@ import akka.stream.javadsl.Source;
  */
 public class Application {
 
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException, InterruptedException {
-		ActorSystem system = ActorSystem.create("$name$");
-		Materializer mat = ActorMaterializer.create(system);
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, InterruptedException {
+        ActorSystem system = ActorSystem.create("$name$");
+        Materializer mat = ActorMaterializer.create(system);
 
-		// Example: This timer source sends a String each second:
-		Source<String, ?> tickSource = Source.tick(Duration.ZERO, Duration.ofSeconds(1), "Tick!");
+        // Example: This timer source sends a String each second:
+        Source<String, ?> tickSource = Source.tick(Duration.ZERO, Duration.ofSeconds(1), "Tick!");
 
-		// This sink will output the values returned straight to the console:
-		Sink<String, ?> stdOutSink = Sink.foreach(o -> System.out.println(o));
+        // This sink will output the values returned straight to the console:
+        Sink<String, ?> stdOutSink = Sink.foreach(o -> System.out.println(o));
 
-		// Streams are built by a Materializer (mat), and go from a source (via a number
-		// of flow steps) to a sink:
+        // Streams are built by a Materializer (mat), and go from a source (via a number
+        // of flow steps) to a sink:
 		tickSource.to(stdOutSink).run(mat);
 
-		// Use RIoT's Builders (e.g. riot.GPIO, riot.I2C) or Akka Streams components
-		// from other projects (e.g. Alpakka) to create additional steps, and use them
-		// in your streams.
+	    // Use RIoT's Builders (e.g. riot.GPIO, riot.I2C) or Akka Streams components
+	    // from other projects (e.g. Alpakka) to create additional steps, and use them
+	    // in your streams.
 
 	}
 
